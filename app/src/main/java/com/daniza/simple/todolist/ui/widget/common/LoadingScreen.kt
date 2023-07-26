dcb39607ed7b843f9c649a2c54d0124c205ff7c9
danizakaria63 <daniootsutsuki@gmail.com>
2023-07-26 20:21:46 +0700

    minor: just changed the loading indicator's color

--- app/src/main/java/com/daniza/simple/todolist/ui/widget/common/LoadingScreen.kt
+++ app/src/main/java/com/daniza/simple/todolist/ui/widget/common/LoadingScreen.kt
@@ -7,12 +7,13 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 @Composable
 fun LoadingScreen() {
     Box(modifier = Modifier.fillMaxSize()) {
         CircularProgressIndicator(
-            color = MaterialTheme.colorScheme.primary,
+            color = Color.White,
             modifier = Modifier.align(Alignment.Center)
         )
     }
